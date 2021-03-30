5684c6a3c37d847f914cbaafe5e46ad7d50d04b8
Amal Chandran <amalchandrandev@gmail.com>
2021-03-30 19:29:19 +0000

    feat(admin): Created react admin

--- app/app-web/src/App.tsx
+++ app/app-web/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Button } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Router, Link } from "@reach/router";
+import LoginPage from "./pages/LoginPage";
+const { Header, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <LoginPage path="/" />
+  </Router>
+);
 
 export default App;
---
+++ app/app-web/src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Router, RouteComponentProps, Link } from "@reach/router";
+export default function LoginPage(props: RouteComponentProps) {
+  return <div>Login</div>;
+}
